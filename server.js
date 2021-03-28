c76235310b8e2a3b5d59cb0f3891db4226e94aa9
Coderc <dev@coderc.in>
2021-03-28 13:48:09 +0530

    Changed response

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/api/", (req, res) => {
-  res.send("This is root route");
+  res.send("This is root route").status(200);
 });
 
 app.get("/api/profile", (req, res) => {
-  res.send("This is profile route");
+  res.send("This is profile route").status(200);
 });
 
 app.get("/api/about", (req, res) => {
-  res.send("This is about route");
+  res.send("This is about route").status(200);
 });
 
 app.listen(5000, () => {
